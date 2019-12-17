14984a6c04014512b0fd8f62785b8c797dfc3c16
Xiaofeng Tan <xiaofeng_tan@live.com>
2019-12-17 14:43:49 +0800

    feat: add solution

---
+++ src/SinaWeiboSDK/SinaWeibo.Core/MemoryFileContent.cs
@@ -0,0 +1,18 @@
+﻿namespace SinaWeibo.Core
+{
+    /// <summary>
+    /// The content of memory file.
+    /// </summary>
+    public class MemoryFileContent
+    {
+        /// <summary>
+        /// File's name.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// The bytes of the file's content.
+        /// </summary>
+        public byte[] Content { get; set; }
+    }
+}
---
+++ src/SinaWeiboSDK/SinaWeibo.Core/OAuthClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace SinaWeibo.Core
+{
+    /// <summary>
+    /// OpenAuth 2.0 client
+    /// </summary>
+    public class OAuthClient
+    {
+        /// <summary>
+        /// HTTP client.
+        /// </summary>
+        private readonly HttpClient _http;
+
+        /// <summary>
+        /// OpenAuth Server Base API URL.
+        /// </summary>
+        public string BaseApiUrl => "https://api.weibo.com/2/";
+
+        /// <summary>
+        /// The address of getting Access Token.
+        /// </summary>
+        public string AccessTokenUrl => "https://api.weibo.com/oauth2/access_token";
+
+        /// <summary>
+        /// The address of getting authorization code.
+        /// </summary>
+        public string AuthorizationCodeUrl => "https://api.weibo.com/oauth2/authorize";
+
+        public OAuthClient()
+        {
+            _http = new HttpClient(new HttpClientHandler
+            {
+                CookieContainer   = new CookieContainer(),
+                UseCookies        = true,
+                AllowAutoRedirect = false
+            }) {BaseAddress = new Uri(BaseApiUrl)};
+        }
+    }
+}
